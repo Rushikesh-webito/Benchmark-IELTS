0e9115fa497fc1418bc9a46debe2ad6c938afded
Rushikesh Antala <rushikesh.antala@webitoinfotech.com>
2024-09-22 19:51:29 +0530

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\{Auth,Hash,Log,Validator};
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+
+    public function __construct()
+    {
+        # By default we are using here auth:api middleware
+        // $this->middleware('api', ['except' => ['login']]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+        
+        if ($validator->fails()) {
+            // die('11');
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation errors',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+        // $token = Auth::login($user);
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = Auth::guard('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    
+        return response()->json(['token' => $token]);
+    }
+
+    public function logout()
+    {
+        auth()->logout(); # This is just logout function that will destroy access token of current user
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        # When access token will be expired, we are going to generate a new one wit this function 
+        # and return it here in response
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        # This function is used to make JSON response with new
+        # access token of current user
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
+
+
